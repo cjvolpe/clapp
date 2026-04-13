f6c2a09f8189077b34a1567735d462a13e1b40be
cjvolpe <cjvolpe@unc.edu>
2026-04-13 02:18:45 -0400

    added simple search and filtered search

--- backend/src/routes.ts
+++ backend/src/routes.ts
@@ -2,28 +2,27 @@
 
 // external
 import type {FastifyInstance} from "fastify";
-import type {BaseReply, Failure, ReplyConfig, Task, Process, Climb} from "../../frontend/src/lib/types.ts";
-import * as url from "node:url";
+import {
+    type BaseReply,
+    type Failure,
+    type ReplyConfig,
+    type Task,
+    type Process,
+    type Climb,
+    type Search,
+    ROPE_GRADES,
+    BOULDER_GRADES
+} from "../../frontend/src/lib/types.ts";
 
-// internal
+const ropeGrades: string[] = Object.keys(ROPE_GRADES);
+const boulderGrades: string[] = Object.keys(BOULDER_GRADES);
 
-
-const exampleFilters: Record<string, any> = {
-    // search: "",
-    // color: "Red",
-    type: "Top Rope"
-}
 //TODO: add post request for claiming a set climb, and ticking a climb
 export function setupRoutes(server: FastifyInstance) {
     server.get<{
         Reply: any[] | { error: string };
     }>("/featured", async (request, reply) => {
-        // for (const key in exampleFilters) {
-        //     query = query.eq(key, exampleFilters[key])
-        // }
-
         const {reply: result, code} = await packageResponse(() => handleFeaturedClimbs());
-
         return reply.status(code).send(result);
     });
     server.post<{
@@ -39,7 +38,22 @@ export function setupRoutes(server: FastifyInstance) {
         const {reply, code} = await packageResponse(() => handleArchive(req.params));
         res.status(code).send(reply);
 
-    })
+    });
+
+    server.post<{
+        Body: Search;
+        Reply: BaseReply<void>;
+    }>("/climbs/search/filter", async (req, res) => {
+        const {reply, code} = await packageResponse(() => handleFilteredSearch(req.body));
+        res.status(code).send(reply);
+    });
+    server.post<{
+        Body: string;
+        Reply: BaseReply<void>;
+    }>("/climbs/search", async (req, res) => {
+        const {reply, code} = await packageResponse(() => handleSearch(req.body));
+        res.status(code).send(reply);
+    });
 
     async function handleFeaturedClimbs(): Promise<Task> {
         const twoWeeksAgo = new Date();
@@ -53,6 +67,7 @@ export function setupRoutes(server: FastifyInstance) {
         return {success: true, data: data};
 
     }
+
 //TODO: Handle pictures & fix date for postman
     async function handleNewClimb(req: Climb): Promise<Task> {
         const {name, difficulty, type, color, setter, dateSet, gym} = req;
@@ -73,14 +88,137 @@ export function setupRoutes(server: FastifyInstance) {
         return {success: true, data: data};
     }
 
-    async function handleArchive(params:{id?: string}): Promise<Task> {
+    async function handleArchive(params: { id?: string }): Promise<Task> {
         const {id} = params;
-        const {data, error} = await server.supabase.from("climbs").update({archived: true}).eq("id",id).select();
+        const {data, error} = await server.supabase.from("climbs").update({archived: true}).eq("id", id).select();
         if (error) {
             return {success: false, error: error, code: 500};
         }
         return {success: true, data: data};
 
+    }
+//TODO: fix difficulty selection
+    async function handleFilteredSearch(req: Search): Promise<Task> {
+        const {name, lowerDifficulty, upperDifficulty, type, setter, color, startDate, endDate, gym, archived} = req;
+        const query = server.supabase.from("climbs").select('*');
+        let gradeLock: boolean = false;
+        const filter: Record<string, any> = {
+            name: name,
+            lowerDifficulty: lowerDifficulty,
+            upperDifficulty: upperDifficulty,
+            type: type,
+            setter: setter,
+            color: color,
+            startDate: startDate,
+            endDate: endDate,
+            gym: gym,
+            archived: archived,
+        }
+        if (filter[lowerDifficulty] !== null && filter[upperDifficulty] !== null) {
+            let gradeList: string[] = [];
+            if (filter["type"] === "Top Rope") {
+                gradeList = Array.from(ropeGrades);
+                console.log(gradeList);
+                gradeList = sortByRopeGradeUpperLower(filter[lowerDifficulty], filter[upperDifficulty], gradeList);
+
+            } else if (filter["type"] === "Boulder") {
+                gradeList = Array.from(boulderGrades);
+                console.log(gradeList + "sdf");
+                gradeList = sortByBoulderGradeUpperLower(filter[lowerDifficulty], filter[upperDifficulty], gradeList);
+            }
+
+
+            console.log(gradeList);
+            query.in("difficulty", gradeList);
+            gradeLock = true;
+            console.log(gradeLock);
+        }
+        for (const key in filter) {
+            if (filter[key] === null) {
+                continue;
+            }
+            if (key === "name") {
+                query.textSearch("name", filter[key], {
+                    config: "english",
+                    type: "websearch"
+                });
+            } else if ((key === "lowerDifficulty" || key === "upperDifficulty") && !gradeLock) {
+                let gradeList: string[] = [];
+                if (filter["type"] === "Top Rope") {
+                    gradeList = Array.from(ropeGrades);
+                    console.log(gradeList);
+                    if (key === "lowerDifficulty") gradeList = sortByRopeGrade("lower", filter[key], gradeList);
+                    if (key === "upperDifficulty") gradeList = sortByRopeGrade("upper", filter[key], gradeList);
+                } else if (filter["type"] === "Boulder") {
+                    gradeList = Array.from(boulderGrades);
+                    console.log(gradeList);
+                    if (key === "lowerDifficulty") gradeList = sortByBoulderGrade("lower", filter[key], gradeList);
+                    if (key === "upperDifficulty") gradeList = sortByBoulderGrade("upper", filter[key], gradeList);
+                }
+                console.log(gradeList);
+                query.in("difficulty", gradeList);
+            } else if (key === "type") {
+                query.eq('type', filter[key]);
+            } else if (key === "color") {
+                query.eq('color', filter[key]);
+            } else if (key === "setter") {
+                query.textSearch("setter", filter[key], {
+                    config: "english",
+                    type: "websearch"
+                });
+            } else if (key === "gym") {
+                query.eq('gym', filter[key]);
+            } else if (key === "archived") {
+                query.eq('archived', filter[key]);
+            } else if (key === "startDate") {
+                query.gte('startDate', filter[key]);
+            } else if (key === "endDate") {
+                query.lte('endDate', filter[key]);
+            }
+        }
+        const {data, error} = await query;
+        gradeLock = false;
+        if (error) {
+            return {success: false, error: error, code: 500};
+        }
+        return {success: true, data: data};
+    }
+
+    async function handleSearch(req: string): Promise<Task> {
+        const searchTerm: string = `${req.search}:*`;
+        console.log(searchTerm);
+        const {
+            data,
+            error
+        } = await server.supabase.from("climbs").select('*').or(`name.wfts.${searchTerm},setter.wfts.${searchTerm}`);
+        if (error) {
+            return {success: false, error: error, code: 500};
+        }
+        return {success: true, data: data};
+    }
+
+    function sortByRopeGrade(bound: string, filterGrade: string, gradeList: string[]) {
+        if (bound === "lower") {
+            return gradeList.filter(grade => ROPE_GRADES[grade] >= ROPE_GRADES[filterGrade]);
+        } else if (bound === "upper") {
+            return gradeList.filter(grade => ROPE_GRADES[grade] <= ROPE_GRADES[filterGrade]);
+        }
+    }
+
+    function sortByBoulderGrade(bound: string, filterGrade: string, gradeList: string[]) {
+        if (bound === "lower") {
+            return gradeList.filter(grade => BOULDER_GRADES[grade] >= BOULDER_GRADES[filterGrade]);
+        } else if (bound === "upper") {
+            return gradeList.filter(grade => BOULDER_GRADES[grade] <= BOULDER_GRADES[filterGrade]);
+        }
+    }
+
+    function sortByRopeGradeUpperLower(lowerFilter: string, upperFilter: string, gradeList: string[]) {
+        return gradeList.filter(grade => ROPE_GRADES[lowerFilter] >= ROPE_GRADES[grade] <= ROPE_GRADES[upperFilter]);
+    }
+
+    function sortByBoulderGradeUpperLower(lowerFilter: string, upperFilter: string, gradeList: string[]) {
+        return gradeList.filter(grade => BOULDER_GRADES[lowerFilter] >= BOULDER_GRADES[grade] <= BOULDER_GRADES[upperFilter]);
     }
 
 
@@ -108,31 +246,4 @@ export function setupRoutes(server: FastifyInstance) {
         throw result.error;
 
     }
-
-//TODO: Replace schema with a getter in the frontend
-//     server.get<{
-//         Reply: any[] | { error: string };
-//     }>("/climb",{schema:{querystring:{properties:{}}}, async (request, reply) => {
-//         let query = server.supabase.from("climbs").select('*');
-//         const {data, error} = await query;
-//         if (error) {
-//             return reply.status(500).send(error);
-//         }
-//         return data;
-//     });
-    // server.post<{
-    //     Body: Task;
-    // }>("/task", async (request, reply) => {
-    //     const {name, description, dueDate} = request.body;
-    //     const {data, error} = await server.supabase.from("task").insert([{
-    //         task_name: name,
-    //         description: description,
-    //         due_date: dueDate
-    //     }]).select();
-    //
-    //     if (error) {
-    //         return reply.status(500).send(error);
-    //     }
-    //     return reply.status(201).send(data);
-    // });
 }
